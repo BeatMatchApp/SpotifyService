1671d01987ead670d4adced00c52c3a23214156e
CarmelBarda <carmelbd78@colman.ac.il>
2025-05-07 16:35:07 +0300

    carmels example

--- src/controllers/general.ts
+++ src/controllers/general.ts
@@ -2,19 +2,45 @@ import axios from 'axios';
 import { Request, Response } from 'express';
 import { SPOTIFY_API_URL } from '../consts/spotify';
 import { generateSpotifyHeaders } from '../consts/auth';
+import { envVariables } from '../config/config';
+
+const CLIENT_ID = envVariables.clientId;
+const CLIENT_SECRET = envVariables.clientSecret;
 
 export const getArtists = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    const artist: string = encodeURIComponent(req.body.query);
-    const response = await axios.get(
-      `${SPOTIFY_API_URL}/search?q=${artist}&type=artist&limit=10`,
-      { headers: generateSpotifyHeaders(req) }
+    const credentials = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+      'base64'
     );
 
-    const data = await response.data;
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      'grant_type=client_credentials',
+      {
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    const access_token = response.data.access_token;
+
+    const artist: string = encodeURIComponent(req.body.query);
+
+    const response2 = await axios.get(
+      `${SPOTIFY_API_URL}/search?q=${artist}&type=artist&limit=10`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const data = await response2.data;
 
     const artists = data.artists.items
       .map((artist: any) => artist.name)
