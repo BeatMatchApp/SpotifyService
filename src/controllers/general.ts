4a01bb7d86d338a2f37bff738fb15f2769b540c1
ayatal <ayatal.712@cs.colman.ac.il>
2025-05-17 12:20:52 +0300

    add genre and song searches

--- src/controllers/general.ts
+++ src/controllers/general.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { Request, Response } from 'express';
 import { SPOTIFY_API_URL } from '../consts/spotify';
 import { generateSpotifyHeaders } from '../consts/auth';
+import { GENRE_SEEDS } from '../consts/general';
+import { SpotifyTrack } from '../models/interfaces/SpotifySearch';
+import { searchTracks } from '../functions/tracks';
 
 export const getArtists = async (
   req: Request,
@@ -10,13 +13,13 @@ export const getArtists = async (
   try {
     const artist: string = encodeURIComponent(req.body.query);
     const response = await axios.get(
-      `${SPOTIFY_API_URL}/search?q=${artist}&type=artist&limit=10`,
+      `${SPOTIFY_API_URL}/search?q=${artist}&type=artist&limit=15`,
       { headers: generateSpotifyHeaders(req) }
     );
 
     const data = await response.data;
 
-    const artists = data.artists.items
+    const artists: string[] = data.artists.items
       .map((artist: any) => artist.name)
       .filter((name: string) =>
         name.toLowerCase().includes(artist.toLowerCase())
@@ -27,5 +30,45 @@ export const getArtists = async (
     res.status(500).json({
       error: `Error searching artist: ${error.response?.data || error}`,
     });
+  }
+};
+
+export const getGenres = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const query: string = encodeURIComponent(
+      req.body.query
+    ).toLocaleLowerCase();
+
+    const filteredGenres = GENRE_SEEDS.filter((genre: string) =>
+      genre.toLocaleLowerCase().startsWith(query)
+    ).slice(0, 14);
+
+    res.json(filteredGenres);
+  } catch (error) {
+    console.error('Error searching genres:', error);
+
+    res.status(500).json({
+      error: `Error getting genres: ${error.message || error}`,
+    });
+  }
+};
+
+export const getSongOptions = async (req: Request, res: Response) => {
+  const query: string = req.body.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Missing search query' });
+  }
+
+  try {
+    const tracks: SpotifyTrack[] = await searchTracks(req, query);
+
+    const results = tracks.map((track: SpotifyTrack) => track.name);
+
+    res.json(results);
+  } catch (error) {
+    console.error('Error searching songs:', error);
+
+    res.status(500).json({ error: 'Failed to search for songs' });
   }
 };
--- src/routes/general.ts
+++ src/routes/general.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getArtists } from '../controllers/general';
+import { getArtists, getGenres, getSongOptions } from '../controllers/general';
 
 const router = express.Router();
 
 router.post('/getArtists', getArtists);
+router.post('/getGenres', getGenres);
+router.post('/getSongs', getSongOptions);
 
 export default router;
