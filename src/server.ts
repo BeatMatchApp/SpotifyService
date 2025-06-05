9d61f97c8dad3c8ca09116c6dc00654e781c833d
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-05 17:13:35 +0300

    https

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,19 @@ import http, { Server } from 'http';
 import fs from 'fs';
 import https from 'https';
 
-console.log('🟢 Spotify server is starting...');
-
 createServer().then((app) => {
   const port: number = envVariables.port;
-  console.log('creating server on port ', port);
 
   let server: Server;
 
   if (envVariables.nodeEnv !== 'production') {
     server = http.createServer(app);
   } else {
-    // const certs = {
-    //   key: fs.readFileSync('./client-key.pem'),
-    //   cert: fs.readFileSync('./client-cert.pem'),
-    // };
-    server = http.createServer(app);
+    const certs = {
+      key: fs.readFileSync('./client-key.pem'),
+      cert: fs.readFileSync('./client-cert.pem'),
+    };
+    server = https.createServer(app);
   }
 
   server = server
