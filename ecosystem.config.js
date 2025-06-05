8bd71ee77a44be4745f422ddd911a597da00dbad
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-05 16:27:04 +0300

    s

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'spotify_server_app',
-      script: './dist/app.js',
+      script: './dist/server.js',
       env_production: {
         NODE_ENV: 'production',
       },
