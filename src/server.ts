2c5b5b405c0d19c4b2307285bc0d3fd78b70031c
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-05 16:16:44 +0300

    cert

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,8 @@ createServer().then((app) => {
     server = http.createServer(app);
   } else {
     const certs = {
-      key: fs.readFileSync('./cert.pem'),
-      cert: fs.readFileSync('./cert.pem'),
+      key: fs.readFileSync('./client-key.pem'),
+      cert: fs.readFileSync('./client-cert.pem'),
     };
     server = https.createServer(certs, app);
   }
