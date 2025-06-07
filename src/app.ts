ee48393759c847d406da6fda60f49be29abfde12
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-06-07 17:26:03 +0300

    const cors = require('cors');

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import cors from 'cors';
 import express, { Express } from 'express';
 import cookieParser from 'cookie-parser';
 import BaseRouter from './routes/index';
 import { callback } from './controllers/auth';
 
-const cors = require("cors");
+const cors = require('cors');
 
 const createServer = async (): Promise<Express> => {
   try {
